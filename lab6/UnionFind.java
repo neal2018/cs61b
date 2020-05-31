a83d9135fe80e3672a096dcef192bd3abc8c9148
neallai <neal.lion@icloud.com>
2020-05-31 23:55:55 +0800

    fin lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,24 +1,29 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    private int[] parents; // the parent of root would be the negative size
 
     /*
      * Creates a UnionFind data structure holding n vertices. Initially, all
      * vertices are in disjoint sets.
      */
     public UnionFind(int n) {
-        // TODO
+        parents = new int[n];
+        for (int i = 0; i < n; ++i) {
+            parents[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex >= parents.length || vertex < 0) {
+            throw new IllegalArgumentException("Invalid Index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return -parents[find(v1)];
     }
 
     /*
@@ -26,14 +31,17 @@ public class UnionFind {
      * size of the tree for which v1 is the root.
      */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        if (parents[v1] >= 0) {
+            parents[v1] = parents[find(v1)];
+        }
+        return parents[find(v1)];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
     /*
@@ -44,7 +52,23 @@ public class UnionFind {
      * may alter the internal structure of the data.
      */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            // no extra path-compression step needed for the case of
+            // connected v1 and v2,
+            // because the connected(v1, v2) is always called whenever
+            // v1 and v2 are connected or not,
+            // because there is find() in connected(),
+            // so path-compression is already applied.
+            if (sizeOf(v1) > sizeOf(v2)) {
+                parents[find(v1)] -= sizeOf(v2);
+                parents[find(v2)] = find(v1);
+            } else {
+                parents[find(v2)] -= sizeOf(v1);
+                parents[find(v1)] = find(v2);
+            }
+        }
     }
 
     /*
@@ -52,8 +76,19 @@ public class UnionFind {
      * allowing for fast search-time.
      */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int root = vertex;
+        while (parent(root) > -1) {
+            root = parent(root);
+        }
+        // path-compression
+        int currParent;
+        while (vertex != root) {
+            currParent = parent(vertex);
+            parents[vertex] = root;
+            vertex = currParent;
+        }
+        return root;
     }
 
 }
